1946a6b9c7a7f9573fd18d899f23667fcca02939
putrabudianto <craftgirlsssshopping@gmail.com>
2024-03-15 18:43:43 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Putra MacBook on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
